37e0ab7e9036ad42b53d13f8ce175fff9c9e75b0
Haritha Senevirathne <harithsenevi4@gmail.com>
2025-01-25 11:49:17 +0000

    initial commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Health Check!");
+});
+
+app.listen(3000);
+console.log("Listening to port 3000");
